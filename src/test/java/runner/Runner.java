76956a1f7bd32725933e8f9bc99b982988241284
Asik Mia <mohammadasik828@gmail.com>
2024-05-26 12:14:39 +0600

    update

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
        features = {"src/test/resources/features"},
        glue = {"stepDefinitions","Hooks"}, tags="@smoke",
+       monochrome = true,
+       dryRun = false,
        plugin = {"pretty", "html:target/cucumber-reports" }
 )
 public class Runner {
